79456336c0dcb8119c14012bc7d0238f3861839e
Elisabeth Garcia <lisgarciae@gmail.com>
2023-04-14 17:55:55 -0400

    passing

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun (activity="roller-skate") {
+ return `This Saturday, I want to ${activity}!`
+}
+console.log(saturdayFun('bathe my dog'))
+//
+
+const mondayWork = function (work= "go to the office") {
+    return `This Monday, I will ${work}.`
+}
+console.log(mondayWork())
+
+//
+function wrapAdjective (result="") {
+    return function(pam1) {
+        return `You are ${result}${pam1}${result}!`
+    }
+    }
+console.log(wrapAdjective("||")("a dedicated programmer"))
